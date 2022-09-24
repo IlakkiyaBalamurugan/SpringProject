eb9b1194125bc37adf94f397c504a4362c798b8c
IlakkiyaBalamurugan <ilakkiya.s.ece@gmail.com>
2022-09-24 09:33:45 -0400

    spring-user added

---
+++ security-user/security-user/src/test/java/com/UserReistration/securityuser/SecurityUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserReistration.securityuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
